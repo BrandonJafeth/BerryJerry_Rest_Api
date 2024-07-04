59ada45154fde374ba219e76634421f5b9b48922
BrandonJafeth <brandon.carrillo.alvarez@est.una.ac.cr>
2024-07-04 16:09:40 -0600

    Conexión con base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,27 @@
 
 import { Module } from '@nestjs/common';
-import { FrutasModule } from './frutas/frutas.module';
 import { HeroModule } from './hero/hero.module';
 import { GalleryModule } from './gallery/gallery.module';
 import { CustomerReviewsModule } from './customer-reviews/customer-reviews.module';
 import { ServicesModule } from './services/services.module';
 import { FooterInfoModule } from './footer-info/footer-info.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
-  imports: [FrutasModule, HeroModule, GalleryModule, CustomerReviewsModule, ServicesModule, FooterInfoModule],
+  imports: [ HeroModule, GalleryModule, CustomerReviewsModule, ServicesModule, FooterInfoModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'Obando19',
+      database: 'company',
+      entities: [],
+      autoLoadEntities: true,
+      synchronize: true,
+    })
+],
+
 })
 export class AppModule {}
